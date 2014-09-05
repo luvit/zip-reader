02c58d0a827d1a0e2ce643f2f9599448f098ba10
Tim Caswell <tim@creationix.com>
2014-09-05 13:09:05 -0500

    Remove unused function

--- test.lua
+++ test.lua
@@ -2,15 +2,6 @@ local loadZip = require('zip')
 local await = require('await')
 local uv = require('luv')
 
-local function tobin(bin)
-  local str = '<'
-  for i=1,#bin do
-    str = str .. bit.tohex(string.byte(bin, i),2) .. " "
-  end
-  str = str .. '>'
-  return str
-end
-
 coroutine.wrap(function ()
   local fd = await(uv.fs_open, uv.execpath(), "r", tonumber("644", 8))
   local zip = assert(loadZip(fd))
